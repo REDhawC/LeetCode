655c8a7f8f15d9533f198604ba05a2de2360af4b
redhawc <redhawc1@gmail.com>
2023-07-03 14:43:22 +0800

    03/07/2023-2

--- 202.happy-number.py
+++ 202.happy-number.py
@@ -8,19 +8,21 @@
 # @lc code=start
 class Solution:
     def isHappy(self, n: int) -> bool:
-        sum = 0
-        if n == 2:
-            return False
-        while sum != 1:
-            happyList = list(str(n))
+        savedNums = set()
+        while True:
             sum = 0
+            happyList = list(str(n))
             for i in happyList:
                 sum += int(i) ** 2
+            if sum in savedNums:
+                return False
+            if sum == 1:
+                return True
             n = sum
-        return True
+            savedNums.add(sum)
 
 
-# strs = 19
-# Solution.isHappy(Solution, strs)
+strs = 19
+Solution.isHappy(Solution, strs)
 
 # @lc code=end
