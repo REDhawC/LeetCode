642e4f830b5026af91300c75b15a44d0ecf19cd8
redhawc <redhawc1@gmail.com>
2023-07-16 14:15:45 +0800

    16/07/2023

---
+++ 114.flatten-binary-tree-to-linked-list.py
@@ -0,0 +1,41 @@
+#
+# @lc app=leetcode id=114 lang=python3
+#
+# [114] Flatten Binary Tree to Linked List
+#
+
+
+# @lc code=start
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def __init__(self) -> None:
+        self.preOrder = []
+
+    def traverse(self, root):
+        if not root:
+            return
+        self.preOrder.append(root)
+        if root.left:
+            self.traverse(root.left)
+        if root.right:
+            self.traverse(root.right)
+
+    def flatten(self, root):
+        self.traverse(root)
+        for i in range(len(self.preOrder) - 1):
+            self.preOrder[i].left = None
+            self.preOrder[i].right = self.preOrder[i + 1]
+
+        """
+        Do not return anything, modify root in-place instead.
+        """
+
+
+# @lc code=end
--- 117.populating-next-right-pointers-in-each-node-ii.py
+++ 117.populating-next-right-pointers-in-each-node-ii.py
@@ -24,54 +24,77 @@ class Node:
 
 class Solution:
     def connect(self, root: "Node") -> "Node":
-        def getValidNode(father):
-            if not father:
-                return None
-            if father.left and father.right:
-                return father.left
-            elif father.left and not father.right:
-                return father.left
-            elif not father.left and father.right:
-                return father.right
-
         if not root:
             return root
-        curLayer = root
-        while curLayer:
-            print(1)
-            flag = 1
-            father = curLayer
-            stack = []
-            while father:
-                print(2)
-                curNode = getValidNode(father)
-                # test valid father-nodes
 
-                if not curNode:
-                    father = father.next
-                if flag and curNode:  # mark first son-node for layer deepening
-                    firstNode = curNode
-                    flag = 0
-                # enter the layer looping
-                while curNode:
-                    print("val:", curNode.val)
-                    print(3)
-                    while not (father.left or father.right):
-                        print(4)
-                        if not father:
-                            break
-                        father = father.next
-                    if curNode == father.left and father.right:
-                        curNode.next = father.right
-                    else:
-                        curNode.next = getValidNode(father)
-                    curNode = curNode.next
+        # cur可以看做是每一层的链表的头结点
+        cur = root
+        while cur:
+            # 为了方便操作在下一层前面添加一个哑结点
+            # pre表示访问下一层节点的前一个节点
+            dummy = Node(0)
+            pre = dummy
+            # 遍历当前层的链表
+            while cur:
+                if cur.left:
+                    # 如果当前节点的左子节点不为空，就让pre节点的next指向它，也就是把它串起来
+                    pre.next = cur.left
+                    # 然后再更新pre
+                    pre = pre.next
+                if cur.right:
+                    # 同理参照左子树
+                    pre.next = cur.right
+                    pre = pre.next
+                # 继续访问这一行的下一个节点
+                cur = cur.next
+            # 把下一层串联成一个链表之后，让它赋值给cur，后续继续循环，直到cur为空为止
+            cur = dummy.next
+        return root
 
-                father = father.next
-            if firstNode:
-                curLayer = firstNode
-            else:
-                return root
+
+# class Solution:
+#     def connect(self, root: "Node") -> "Node":
+#         def getValidNode(father):
+#             if not father:
+#                 return None
+#             if father.left and father.right:
+#                 return father.left
+#             elif father.left:
+#                 return father.left
+#             elif father.right:
+#                 return father.right
+
+#         if not root:
+#             return root
+#         curLayer = root
+#         while curLayer:
+#             flag = 1
+#             father = curLayer
+#             stack = []
+#             while father:
+#                 curNode = getValidNode(father)
+#                 if not curNode:
+#                     father = father.next
+#                 if flag and curNode:
+#                     firstNode = curNode
+#                     flag = 0
+#                 while curNode:
+#                     while not (father.left or father.right):
+#                         if not father.next:
+#                             break
+#                         father = father.next
+#                     if curNode == father.left and father.right:
+#                         curNode.next = father.right
+#                     else:
+#                         curNode.next = getValidNode(father)
+#                     curNode = curNode.next
+
+#                 father = father.next
+#             if flag:
+#                 return root
+#             curLayer = firstNode
+
+#         return root
 
 
 # @lc code=end
